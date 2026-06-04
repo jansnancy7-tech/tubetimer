1b2b2728ad744f342b261b308408393af7f61702
Jans Nancy <jansnancy7@gmail.com>
2026-06-04 15:37:28 +0200

    php app added

---
+++ public/index.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+$page = $_GET['page'] ?? 'home';
+
+switch ($page) {
+    case 'home':
+        echo '<h1>Video Timer Scheduler</h1>';
+        echo '<p>Welcome to the project.</p>';
+        break;
+
+    default:
+        http_response_code(404);
+        echo '<h1>404 Not Found</h1>';
+}
